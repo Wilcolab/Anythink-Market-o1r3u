9f7ac48dff52bfefebcfb5adf7e248f6efc62918
Jake Lam Jiong Le <64318280+daily-dev-jake@users.noreply.github.com>
2023-05-31 14:59:53 +0000

    edit to add id as search-box

--- frontend/src/components/Home/Banner.js
+++ frontend/src/components/Home/Banner.js
@@ -6,13 +6,13 @@ const Banner = (props) => {
   const [message, setMessage] = React.useState("");
 
   const onSearchTitleChange = (event) => {
-    
-    if(message.length >= 2){
+
+    if (message.length >= 2) {
       props.onSearchFilter(
         event.target.value,
         (page) => agent.Items.byTitle(event.target.value),
         agent.Items.byTitle(event.target.value)
-        
+
       )
     }
     setMessage(event.target.value);
@@ -27,16 +27,18 @@ const Banner = (props) => {
         <div style={{ display: "flex", alignItems: "center", justifyContent: "center" }}>
           <span>A place to <span id="get-part">get</span></span>
           <form>
-            <input 
-              onChange={onSearchTitleChange} 
-              value={message} 
+            <input
+              id="search-box"
+              onChange={onSearchTitleChange}
+              value={message}
               placeholder="What is it you truly desire?"
-              style={{ 
-                width: "15rem", 
-                margin: "0px 10px", 
-                borderRadius: "5px", 
-                borderWidth: "0" }} />
-            </form>
+              style={{
+                width: "15rem",
+                margin: "0px 10px",
+                borderRadius: "5px",
+                borderWidth: "0"
+              }} />
+          </form>
           <span> the cool stuff</span>
         </div>
       </div>
